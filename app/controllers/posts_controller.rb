5fb22ecb22071844919df170f4d21e2fe211eb36
Mitchell Pople <mitchell.pople@gmail.com>
2019-06-14 10:44:58 -0400

    Created User controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,23 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end
+    def show
+        @posts = Post.find(params[:id])
+    end
+    def new
+        @post = Post.new
+    end
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to @post, notice: "Successfully created post"
+        else
+            render :new
+        end
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :summary, :body)
+    end
 end
